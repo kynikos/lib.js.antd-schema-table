62b097714b5d2cac1379a9277d9523dddcdf1120
Dario Giovannetti <dev@dariogiovannetti.net>
2019-10-24 00:46:21 +0800

    Move index.js into src/

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// This file is part of antd-schema-table
+// Copyright (C) 2018-present Dario Giovannetti <dev@dariogiovannetti.net>
+// Licensed under MIT
+// https://github.com/kynikos/lib.js.antd-schema-table/blob/master/LICENSE
+
+export {FieldAuxiliary} from './FieldAuxiliary'
+export {FieldString} from './FieldString'
+export {FieldBooleany} from './FieldBooleany'
+export {FieldBooleanyNull} from './FieldBooleanyNull'
+export {FieldList} from './FieldList'
+export {FieldChoice} from './FieldChoice'
+export {FieldNumber} from './FieldNumber'
+export {FieldDateTime} from './FieldDateTime'
+export {Schema} from './Schema'
+export {SchemaField} from './SchemaField'
+export {SchemaFieldGroup} from './SchemaFieldGroup'
+export {Table} from './Table'
+export {List} from './List'
+export {TableResponsive} from './TableResponsive'
